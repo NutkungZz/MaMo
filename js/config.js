7604c87316ab776997dae460c079b9b25c6d2236
NutkungZz <131230214+NutkungZz@users.noreply.github.com>
2024-10-13 16:45:46 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,23 +5,76 @@ export const RATE_DETAILS = {
     '10': {
         name: 'บ้านอยู่อาศัยขนาดเล็ก',
         tiers: [
-            { limit: 15, price: 2.3488 },
-            { limit: 10, price: 2.9882 },
-            { limit: 10, price: 3.2405 },
-            { limit: 65, price: 3.6237 },
-            { limit: 50, price: 3.7171 },
-            { limit: 250, price: 4.2218 },
-            { limit: Infinity, price: 4.4217 }
+            { limit: 15, prices: [2.3488, 0, 0, 0] },
+            { limit: 10, prices: [2.9882, 0, 0, 0] },
+            { limit: 10, prices: [3.2405, 0, 0, 0] },
+            { limit: 65, prices: [3.6237, 0, 0, 0] },
+            { limit: 50, prices: [3.7171, 0, 0, 0] },
+            { limit: 250, prices: [4.2218, 0, 0, 0] },
+            { limit: Infinity, prices: [4.4217, 0, 0, 0] }
         ],
         serviceCharge: 8.19
     },
     '11': {
         name: 'บ้านอยู่อาศัยขนาดใหญ่',
         tiers: [
-            { limit: 150, price: 3.2484 },
-            { limit: 250, price: 4.2218 },
-            { limit: Infinity, price: 4.4217 }
+            { limit: 150, prices: [3.2484, 0, 0, 0] },
+            { limit: 250, prices: [4.2218, 0, 0, 0] },
+            { limit: Infinity, prices: [4.4217, 0, 0, 0] }
         ],
         serviceCharge: 24.62
-    }
+    },
+    '20': {
+        name: 'กิจการขนาดเล็ก 22-33 kV',
+        tiers: [
+            { limit: Infinity, prices: [3.0202, 0.3419, 0.3009, -0.2456] }
+        ],
+        serviceCharge: 312.24
+    },
+    '21': {
+        name: 'กิจการขนาดเล็ก < 22 kV',
+        tiers: [
+            { limit: 150, prices: [3.2484, 0, 0, 0] },
+            { limit: 250, prices: [4.2218, 0, 0, 0] },
+            { limit: Infinity, prices: [4.4217, 0, 0, 0] }
+        ],
+        serviceCharge: 33.29
+    },
+    '60': {
+        name: 'องค์การไม่แสวงหากำไร > 69 kV',
+        tiers: [
+            { limit: Infinity, prices: [3.0194, 0.3636, 0.1875, -0.1556] }
+        ],
+        serviceCharge: 312.24
+    },
+    '61': {
+        name: 'องค์การไม่แสวงหากำไร 22-33 kV',
+        tiers: [
+            { limit: Infinity, prices: [3.0488, 0.3759, 0.3297, -0.1695] }
+        ],
+        serviceCharge: 312.24
+    },
+    '62': {
+        name: 'องค์การไม่แสวงหากำไร < 22 kV',
+        tiers: [
+            { limit: 10, prices: [2.8013, 0, 0, 0] },
+            { limit: Infinity, prices: [3.8919, 0, 0, 0] }
+        ],
+        serviceCharge: 20.00
+    },
+    '70': {
+        name: 'สูบน้ำเพื่อการเกษตร',
+        tiers: [
+            { limit: 100, prices: [2.0889, 0, 0, 0] },
+            { limit: Infinity, prices: [3.2405, 0, 0, 0] }
+        ],
+        serviceCharge: 115.16
+    },
+    '80': {
+        name: 'ไฟฟ้าชั่วคราว',
+        tiers: [
+            { limit: Infinity, prices: [6.8025, 0, 0, 0] }
+        ],
+        serviceCharge: 0.00
+    },
 };
